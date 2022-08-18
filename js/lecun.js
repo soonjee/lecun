a3fff2ed7ebe837666cd085bd259fbd0c0ef7994
seungji Choi <youinxxs@naver.com>
2022-08-19 06:14:29 +0900

    20220819

--- js/lecun.js
+++ js/lecun.js
@@ -21,12 +21,10 @@ $(function () {
     });
 
 
-
-    // $('.Header').off('mouseenter mouseleave');
-
-
-
     $('.IntroSilder').slick({
+        dots: false,
+        arrows: false,
+        // autoplay: false,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
@@ -34,9 +32,71 @@ $(function () {
         variableWidth: true
     })
 
+    // section01_Intro
+
+    $('.IntroSilder').on('init afterChange', function (e, s, c) {
+        var current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        $('.mainArrow i').removeClass('on')
+        if (c === 1) {
+            $('.mainArrow i').addClass('on')
+        }
+
+        $('.slideNum').text((c ? (c + 1) : 1) + " / " + s.slideCount);      
+    });
+
+
+    $('.mainArrow i:nth-child(1)').on('click', function () {
+        $('.IntroSilder').slick('slickPrev')
+    });
+    $('.mainArrow i:nth-child(2)').on('click', function () {
+        $('.IntroSilder').slick('slickNext')
+    });
+
+
+    $('.IntroSilder').on('afterChange', function (e, s, c) {
+        let t = c % 3;
+
+        $('.IntroBottom .bar span').css({
+           left: t * 33.3333333333 + '%'
+        });
+
+    })
+
+
+    //section04_MenuWrap
+    
     $('.MWSilder').slick({
         dots: true,
-        arrows: true,
+        arrows: false,
+        slidesToShow : 1,		// 한 화면에 보여질 컨텐츠 개수
+		slidesToScroll : 4,
+        centerMode: true,
+        variableWidth: true,
+
+        customPaging : function (slider, i) { 
+            console.log($(slider.$slides[i]).html());
+            return '<button class="tab">' + $(slider.$slides[i]).find('.item').attr('data-dot-title') + '</button>';
+        },
+      
+        // MWSilder__init();
+        //https://codepen.io/kimyangsun/pen/QWbLZGy
     })
+
+    
+    $('.MenuArrow i:nth-child(1)').on('click', function () {
+        $('.MWSilder').slick('slickPrev')
+    });
+    $('.MenuArrow i:nth-child(2)').on('click', function () {
+        $('.MWSilder').slick('slickNext')
+    });
+
+ 
+    // $('.MWSilder').slick({
+    //     customPaging: function(slider, i) { 
+    //         console.log($(slider.$slides[i]).html());
+    //         return '<button class="tab">' + $(slider.$slides[i]).find('.item').attr('data-dot-title') + '</button>';
+    //     },
+    // });
 
 })
